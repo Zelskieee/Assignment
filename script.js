9d77c088df7ba761f0d7580559b9e5f631fb3364
Mohamad Arif Azinuddin Bin Zaidi <chojjaarif2002@gmail.com>
2023-12-08 19:03:26 +0800

    first commit

---
+++ script.js
@@ -0,0 +1,7 @@
+function showPurchaseAlert() {
+    alert("Thank you for purchasing Baseus F01B Tri-Mode Wireless Mouse with us!");
+}
+
+function showAddToCartAlert() {
+    alert("Baseus F01B Tri-Mode Wireless Mouse added to cart!");
+}
